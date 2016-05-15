691918a33d00097f2797ef7e928085fc597c024a
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2016-05-15 22:42:50 +0200

    Extract pixel from image.

--- docs/code/cv/0001.py
+++ docs/code/cv/0001.py
@@ -7,5 +7,16 @@ if __name__ == '__main__':
 
     image = cv2.imread(imagepath)
 
+    image_el = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    ret, thresh1 = cv2.threshold(image_el, 127, 255, cv2.THRESH_BINARY)
+
+    print thresh1
+    for row in thresh1:
+        for pixel in row:
+            output = 'x' if not pixel > 0 else ' '
+            sys.stdout.write(output)
+
+        sys.stdout.write('\n')
+
     cv2.imshow(imagepath, image)
     cv2.waitKey(0)
--- docs/cv.md
+++ docs/cv.md
@@ -1,4 +1,5 @@
 # Computer Vision
 
  - [OpenCV documentation](http://docs.opencv.org/2.4.12/)
+ - [OpenCV python tutorials](https://opencv-python-tutroals.readthedocs.io/en/latest/)
  - [Understanding an Image](http://www.weheartcv.com/understanding-image/)
