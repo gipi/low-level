adb52c9546e2b8e98924cdef379019ab07fbe9c9
Gianluca Pacchiella <gp@ktln2.org>
2016-05-16 12:08:23 +0200

    Find qrcode block size.

--- docs/code/cv/0001.py
+++ docs/code/cv/0001.py
@@ -1,6 +1,27 @@
 import sys
 import cv2
 
+def find_block_size(image):
+    '''Search for the minimum distance between change of data.
+
+    We iterate only the row, since at least one single block exists.
+    '''
+    minimum = image.shape[0]
+
+    for row in image:
+        counter = 0
+        last_bit = None
+        for pixel in row:
+            if last_bit is not None: # this is not the first bit
+                if pixel != last_bit:
+                    minimum = minimum if counter > minimum else counter
+                    counter = 0
+
+            last_bit = pixel
+            counter += 1
+
+    return minimum
+
 
 if __name__ == '__main__':
     imagepath = sys.argv[1]
@@ -19,4 +40,7 @@ if __name__ == '__main__':
         sys.stdout.write('\n')
 
     cv2.imshow(imagepath, image)
+
+    print 'image shape:', image.shape
+    print 'block size:', find_block_size(thresh1)
     cv2.waitKey(0)
