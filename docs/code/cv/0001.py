7f7da0ab8c533804dd65271d615d5380d5753001
Gianluca Pacchiella <gp@ktln2.org>
2016-05-16 14:34:50 +0200

    Extract bits from the image.

--- docs/code/cv/0001.py
+++ docs/code/cv/0001.py
@@ -22,6 +22,18 @@ def find_block_size(image):
 
     return minimum
 
+def bitify(image, block_size):
+    '''transform the image in a simple array with the bits of the single blocks'''
+    def _iter_blocks(_image):
+        for y in xrange(_image.shape[1]/block_size):
+            for x in xrange(_image.shape[0]/block_size):
+                # I don't know why the x and y are swapped below
+                block = _image[y*block_size:y*block_size + block_size, x*block_size:x*block_size + block_size]
+
+                yield block
+
+    return _iter_blocks(image)
+
 
 if __name__ == '__main__':
     imagepath = sys.argv[1]
@@ -40,7 +52,17 @@ if __name__ == '__main__':
         sys.stdout.write('\n')
 
     cv2.imshow(imagepath, image)
-
+    block_size = find_block_size(thresh1)
     print 'image shape:', image.shape
-    print 'block size:', find_block_size(thresh1)
+    print 'block size:', block_size
+
+    bits = [_ for _ in bitify(thresh1, block_size)]
+    signs = [' ' if _[0,0] != 0 else 'x'  for _ in bits]
+
+    width = image.shape[0]/block_size
+
+    for _ in xrange(width):
+        sign = signs[_*width:_*width+width]
+        sys.stdout.write(''.join(sign))
+        sys.stdout.write('\n')
     cv2.waitKey(0)
